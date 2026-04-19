93cc59034e66dea513511b442314469225f4dbfb
Juan Puelpan <juan@puelpan.com>
2026-04-19 15:46:28 -0400

    feat: loading fonts for calendar days

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ clean:
 	rm ./calendar
 
 build:
-	gcc -Wall -Wextra -g -o calendar main.c `pkg-config --cflags --libs sdl2`
+	gcc -Wall -Wextra -g -o calendar main.c `pkg-config --cflags --libs sdl2 SDL2_ttf fontconfig`
 
 run:
 	./calendar
--- main.c
+++ main.c
@@ -1,49 +1,132 @@
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_log.h>
-#include <SDL2/SDL_pixels.h>
-#include <SDL2/SDL_render.h>
-#include <SDL2/SDL_video.h>
+#include <SDL2/SDL_ttf.h>
+#include <fontconfig/fontconfig.h>
 #include <stdbool.h>
 #include <stdio.h>
 
-SDL_Color bgColor = {.r = 0, .g = 0, .b = 0};
+SDL_Color bgColor = {.r = 0, .g = 0, .b = 0, .a = SDL_ALPHA_OPAQUE};
 SDL_Color fgColor = {.r = 255, .g = 255, .b = 255, .a = SDL_ALPHA_OPAQUE};
+SDL_Texture *textures[31];
 
 typedef struct {
   bool running;
-  int initialWidth;
-  int initialHeight;
+  int w;
+  int h;
   SDL_Renderer *renderer;
   SDL_Window *window;
 } AppState;
 
 void CloseApp(AppState *state) { state->running = false; }
 
-void DrawGrid(AppState *app) {
-  int w, h;
-  SDL_GetWindowSize(app->window, &w, &h);
-  float blockWidth = (float)w / 7.0f;
-  float blockHeight = (float)h / 5.0f;
+bool LoadFont(void *buff, char *fontName, size_t buffSize) {
+  FcConfig *config = NULL;
+
+  if (!FcInit()) {
+    SDL_Log("Failed to initialize FontConfig\n");
+    return false;
+  }
+
+  config = FcInitLoadConfigAndFonts();
+  if (config == NULL) {
+    SDL_Log("Failed to load FontConfig configuration\n");
+    return false;
+  }
+
+  if (fontName == NULL || strlen(fontName) == 0) {
+    fontName = "Mono:style=Bold";
+  }
+
+  FcResult result;
+  FcPattern *pat = FcNameParse((const FcChar8 *)fontName);
+
+  FcConfigSubstitute(config, pat, FcMatchKindBegin);
+  FcDefaultSubstitute(pat);
+  SDL_Log("Looking for font: %s\n", fontName);
+
+  FcPattern *font = FcFontMatch(config, pat, &result);
+  if (font) {
+    FcChar8 *file = NULL;
+    if (FcPatternGetString(font, FC_FILE, 0, &file) == FcResultMatch) {
+      memcpy(buff, file, buffSize);
+    }
+  }
+
+  FcPatternDestroy(font);
+  FcPatternDestroy(pat);
+  FcConfigDestroy(config);
+  FcFini();
+
+  SDL_Log("Font found: %s\n", (char *)buff);
+  return true;
+}
+
+int LoadTextures(SDL_Renderer *renderer) {
+  char fontPath[500];
+  if (!LoadFont(&fontPath, NULL, sizeof(fontPath))) {
+    SDL_Log("Failed to load font\n");
+    return 1;
+  }
+
+  if (TTF_Init() < 0) {
+    SDL_Log("Failed to initialize SDL_ttf: %s\n", TTF_GetError());
+    return 1;
+  }
+
+  TTF_Font *font = TTF_OpenFont(fontPath, 1000);
+  if (font == NULL) {
+    SDL_Log("Failed to open font: %s\n", TTF_GetError());
+    return 1;
+  }
+
+  for (int i = 0; i < 31; i++) {
+    char content[10];
+    sprintf(content, "%0d", i);
+
+    SDL_Surface *text = TTF_RenderText_Blended(font, content, fgColor);
+    if (!text) {
+      SDL_Log("Failed to texturize number: %s\n", TTF_GetError());
+      return 1;
+    }
+
+    textures[i] = SDL_CreateTextureFromSurface(renderer, text);
+    SDL_FreeSurface(text);
+  }
+
+  return 0;
+}
+
+void RenderGrid(AppState *state) {
+  float blockWidth = (float)state->w / 7.0f;
+  float blockHeight = (float)state->h / 5.0f;
 
   /* SDL_RenderSetScale(app->renderer, 2.0f, 2.0f); */
-  SDL_SetRenderDrawColor(app->renderer, fgColor.r, fgColor.g, fgColor.b,
+  SDL_SetRenderDrawColor(state->renderer, fgColor.r, fgColor.g, fgColor.b,
                          fgColor.a);
 
-  for (int i = 1; i < 7; i++) {
+  for (int i = 0; i <= 7; i++) {
     int offsetX = (int)blockWidth * i + 1;
-    SDL_RenderDrawLine(app->renderer, offsetX, 0, offsetX, h);
+    SDL_RenderDrawLine(state->renderer, offsetX, 0, offsetX, state->h);
   }
 
-  for (int i = 1; i < 5; i++) {
+  for (int i = 0; i <= 5; i++) {
     int offsetY = (int)blockHeight * i + 1;
-    SDL_RenderDrawLine(app->renderer, 0, offsetY, w, offsetY);
+    SDL_RenderDrawLine(state->renderer, 0, offsetY, state->w, offsetY);
   }
+}
+
+void RenderDays(AppState *state) {
+  float blockWidth = (float)state->w / 7.0f;
+  float blockHeight = (float)state->h / 5.0f;
+
+  SDL_Rect r = {.x = 0, .y = 0, .w = blockWidth, .h = blockHeight};
+
+  SDL_RenderCopy(state->renderer, textures[10], NULL, &r);
 }
 
 int main() {
   printf("Initializing calendar\n");
 
-  AppState state = {.running = true, .initialWidth = 600, .initialHeight = 600};
+  AppState state = {.running = true, .w = 600, .h = 600};
 
   if (SDL_Init(SDL_INIT_VIDEO) != 0) {
     SDL_Log("Failed to initialize SDL: %s\n", SDL_GetError());
@@ -51,8 +134,8 @@ int main() {
   }
 
   state.window = SDL_CreateWindow("Calendar", SDL_WINDOWPOS_CENTERED,
-                                  SDL_WINDOWPOS_CENTERED, state.initialWidth,
-                                  state.initialHeight, SDL_WINDOW_RESIZABLE);
+                                  SDL_WINDOWPOS_CENTERED, state.w, state.h,
+                                  SDL_WINDOW_RESIZABLE);
   if (state.window == NULL) {
     SDL_Log("Failed to create window: %s\n", SDL_GetError());
     return 1;
@@ -62,6 +145,11 @@ int main() {
       SDL_CreateRenderer(state.window, -1, SDL_RENDERER_ACCELERATED);
   if (state.renderer == NULL) {
     SDL_Log("Failed to create renderer: %s\n", SDL_GetError());
+    return 1;
+  }
+
+  if (LoadTextures(state.renderer) != 0) {
+    SDL_Log("Failed to load textures\n");
     return 1;
   }
 
@@ -87,8 +175,10 @@ int main() {
     SDL_SetRenderDrawColor(state.renderer, bgColor.r, bgColor.g, bgColor.b,
                            bgColor.a);
     SDL_RenderClear(state.renderer);
+    SDL_GetWindowSize(state.window, &state.w, &state.h);
 
-    DrawGrid(&state);
+    RenderGrid(&state);
+    RenderDays(&state);
 
     SDL_RenderPresent(state.renderer);
     SDL_Delay(10);
